a070091ce0b3b93c67e47c0d2d4b7e0388ee3e7c
Tara <32590508+talyke@users.noreply.github.com>
2021-03-09 12:44:14 -0700

    Update Painting (2018_04_27 16_31_17 UTC).java
    format edit

--- Painting/src/painting/Painting (2018_04_27 16_31_17 UTC).java
+++ Painting/src/painting/Painting (2018_04_27 16_31_17 UTC).java
@@ -85,58 +85,56 @@ showStatus( "pointer has drifted away from serene painting" );
    {
       Point mp = this.getMousePosition(true);
       x = mp.x;
-      y= mp.y;
+      y = mp.y;
 
      if ( x >= paintingImage.getIconWidth() || y >= paintingImage.getIconHeight() )
          return ("");
   int iconNumber = 5;
-  if (x>0 && x<1054 && y>0 && y<320)
+  if (x> 0 && x < 1054 && y > 0 && y < 320) //Image Grid Measurements
       {
-         iconNumber=0; // SKY
-         if (currentSound >0)
+         iconNumber = 0; // The SKY
+         if (currentSound > 0)
             sounds[currentSound].stop();
-         if (currentSound !=0)
+         if (currentSound != 0)
             sounds[0].play();
          currentSound = 0;
       }
-      if (x>378 && x<1049 && y>321 && y<613)
+      if (x > 378 && x  <1049 && y > 321 && y < 613)
       {
-         iconNumber=1; // WATER
-         if (currentSound >=0 && currentSound !=1)
+         iconNumber = 1; // The WATER
+         if (currentSound >= 0 && currentSound != 1)
             sounds[currentSound].stop();
          if (currentSound !=1)
             sounds[1].play();
          currentSound = 1;
       }
-      if (x>808 && x<912 && y>207 && y<280)
+      if (x > 808 && x < 912 && y > 207 && y < 280)
       {
-         iconNumber=2; // SUN
-         if (currentSound >=0  && currentSound !=2)
+         iconNumber = 2; // The SUN
+         if (currentSound >= 0  && currentSound != 2)
             sounds[currentSound].stop();
-         if (currentSound !=2)
+         if (currentSound != 2)
             sounds[2].play();
          currentSound = 2;
       }
-      if (x>33 && x<378 && y>322 && y<698)
+      if (x > 33 && x < 378 && y > 322 && y < 698)
       {
-         iconNumber=3; //TREE
-         if (currentSound >=0  && currentSound !=3)
+         iconNumber = 3; //The TREE
+         if (currentSound >= 0  && currentSound != 3)
             sounds[currentSound].stop();
-         if (currentSound !=3)
+         if (currentSound != 3)
             sounds[3].play();
          currentSound = 3;
       }
-      if (x>502 && x<570 && y>420 && y<490)
+      if (x > 502 && x < 570 && y > 420 && y < 490)
       {
-         iconNumber=4; // BOAT
-         if (currentSound >=0  && currentSound !=4)
+         iconNumber = 4; // The BOAT
+         if (currentSound >= 0  && currentSound != 4)
             sounds[currentSound].stop();
-         if (currentSound !=4)
+         if (currentSound != 4)
             sounds[4].play();
          currentSound = 4; 
   return captions[ iconNumber ] + "   " + x + "," + y;
   
-        
-
       }
 }
